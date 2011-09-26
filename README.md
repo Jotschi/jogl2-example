7ccf9d680742d60d5253c5004b5dad2518fa2190
Johannes Schüth <git@jotschi.de>
2011-09-26 23:44:15 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+This example shows how to use jogl 2 (v2.0-rc3) http://jogamp.org/deployment/v2.0-rc3/ with maven on linux x86 (i586)
+
---
+++ src/main/java/de/jotschi/jogl2/example/JOGLQuad.java
@@ -0,0 +1,164 @@
+package de.jotschi.jogl2.example;
+
+import java.awt.Component;
+import java.awt.Frame;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.lang.reflect.Field;
+import java.util.Arrays;
+
+import javax.media.opengl.GL;
+import javax.media.opengl.GL2;
+import javax.media.opengl.GL2ES1;
+import javax.media.opengl.GLAutoDrawable;
+import javax.media.opengl.GLEventListener;
+import javax.media.opengl.awt.GLCanvas;
+import javax.media.opengl.fixedfunc.GLLightingFunc;
+import javax.media.opengl.fixedfunc.GLMatrixFunc;
+import javax.media.opengl.glu.GLU;
+
+import com.jogamp.common.jvm.JNILibLoaderBase;
+import com.jogamp.gluegen.runtime.NativeLibLoader;
+import com.jogamp.opengl.util.Animator;
+
+/**
+ * ported to JOGL 2.0 by Julien Gouesse (http://tuer.sourceforge.net)
+ */
+public class JOGLQuad implements GLEventListener, KeyListener {
+
+	static {
+		ClassPathLoader loader = new ClassPathLoader();
+		loader.loadLibrary("gluegen-rt", true);
+		JNILibLoaderBase.setLoadingAction(loader);
+		NativeLibLoader.disableLoading();
+	}
+
+	float rotateT = 0.0f;
+
+	static GLU glu = new GLU();
+
+	static GLCanvas canvas = new GLCanvas();
+
+	static Frame frame = new Frame("Jogl Quad drawing");
+
+	static Animator animator = new Animator(canvas);
+
+	public void display(GLAutoDrawable gLDrawable) {
+		final GL2 gl = gLDrawable.getGL().getGL2();
+		gl.glClear(GL.GL_COLOR_BUFFER_BIT);
+		gl.glClear(GL.GL_DEPTH_BUFFER_BIT);
+		gl.glLoadIdentity();
+		gl.glTranslatef(0.0f, 0.0f, -5.0f);
+
+		// rotate on the three axis
+		gl.glRotatef(rotateT, 1.0f, 0.0f, 0.0f);
+		gl.glRotatef(rotateT, 0.0f, 1.0f, 0.0f);
+		gl.glRotatef(rotateT, 0.0f, 0.0f, 1.0f);
+
+		// Draw A Quad
+		gl.glBegin(GL2.GL_QUADS);
+		gl.glColor3f(0.0f, 1.0f, 1.0f); // set the color of the quad
+		gl.glVertex3f(-1.0f, 1.0f, 0.0f); // Top Left
+		gl.glVertex3f(1.0f, 1.0f, 0.0f); // Top Right
+		gl.glVertex3f(1.0f, -1.0f, 0.0f); // Bottom Right
+		gl.glVertex3f(-1.0f, -1.0f, 0.0f); // Bottom Left
+		// Done Drawing The Quad
+		gl.glEnd();
+
+		// increasing rotation for the next iteration
+		rotateT += 0.2f;
+	}
+
+	public void displayChanged(GLAutoDrawable gLDrawable, boolean modeChanged,
+			boolean deviceChanged) {
+	}
+
+	public void init(GLAutoDrawable gLDrawable) {
+		GL2 gl = gLDrawable.getGL().getGL2();
+		gl.glShadeModel(GLLightingFunc.GL_SMOOTH);
+		gl.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+		gl.glClearDepth(1.0f);
+		gl.glEnable(GL.GL_DEPTH_TEST);
+		gl.glDepthFunc(GL.GL_LEQUAL);
+		gl.glHint(GL2ES1.GL_PERSPECTIVE_CORRECTION_HINT, GL.GL_NICEST);
+		((Component) gLDrawable).addKeyListener(this);
+	}
+
+	public void reshape(GLAutoDrawable gLDrawable, int x, int y, int width,
+			int height) {
+		GL2 gl = gLDrawable.getGL().getGL2();
+		if (height <= 0) {
+			height = 1;
+		}
+		float h = (float) width / (float) height;
+		gl.glMatrixMode(GLMatrixFunc.GL_PROJECTION);
+		gl.glLoadIdentity();
+		glu.gluPerspective(50.0f, h, 1.0, 1000.0);
+		gl.glMatrixMode(GLMatrixFunc.GL_MODELVIEW);
+		gl.glLoadIdentity();
+	}
+
+	public void keyPressed(KeyEvent e) {
+		if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
+			exit();
+		}
+	}
+
+	public void keyReleased(KeyEvent e) {
+	}
+
+	public void keyTyped(KeyEvent e) {
+	}
+
+	public static void exit() {
+		animator.stop();
+		frame.dispose();
+		System.exit(0);
+	}
+
+	public static void main(String[] args) throws SecurityException,
+			NoSuchFieldException, IllegalArgumentException,
+			IllegalAccessException {
+
+		canvas.addGLEventListener(new JOGLQuad());
+		frame.add(canvas);
+		frame.setSize(640, 480);
+		frame.setUndecorated(true);
+		frame.setExtendedState(Frame.MAXIMIZED_BOTH);
+		frame.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				exit();
+			}
+		});
+		frame.setVisible(true);
+		animator.start();
+		canvas.requestFocus();
+	}
+
+	public static void addLibraryPath(String pathToAdd) throws Exception {
+		final Field usrPathsField = ClassLoader.class
+				.getDeclaredField("usr_paths");
+		usrPathsField.setAccessible(true);
+
+		// get array of paths
+		final String[] paths = (String[]) usrPathsField.get(null);
+
+		// check if the path to add is already present
+		for (String path : paths) {
+			if (path.equals(pathToAdd)) {
+				return;
+			}
+		}
+
+		// add the new path
+		final String[] newPaths = Arrays.copyOf(paths, paths.length + 1);
+		newPaths[newPaths.length - 1] = pathToAdd;
+		usrPathsField.set(null, newPaths);
+	}
+
+	public void dispose(GLAutoDrawable gLDrawable) {
+		// do nothing
+	}
+}
